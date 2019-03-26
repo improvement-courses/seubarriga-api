3ab02caa5a1a2f37096c0d34b55fbb6e4031c5e3
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-03-26 17:41:07 -0300

    Deve listar os usuários

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const users = [
+    { name: 'John Doe' },
+  ];
+  res.status(200).send(users);
+});
+
 module.exports = app;
