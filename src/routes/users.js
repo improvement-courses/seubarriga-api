83e3802acaf251d5b4858af82450bf7882bcc231
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-03-28 16:11:34 -0300

    Devo gerenciar erros de uma forma genérica

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,15 @@
 module.exports = (app) => {
-  const findAll = (req, res) => {
+  const findAll = (req, res, next) => {
     app.services.user.findAll()
-      .then(result => res.status(200).send(result));
+      .then(result => res.status(200).send(result))
+      .catch(err => next(err));
   };
-  const create = async (req, res) => {
+  const create = async (req, res, next) => {
     try {
       const result = await app.services.user.save(req.body);
       return res.status(201).json(result[0]);
-    } catch (error) {
-      return res.status(400).json({ error: error.message });
+    } catch (err) {
+      return next(err);
     }
   };
 
