9611db23e00a1309774ed7fc3b8faa894536a13b
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-04-16 18:05:45 -0300

    Ao tentar salvar uma transferência inválida...

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -1,4 +1,4 @@
-// const ValidationError = require('../errors/ValidationError');
+const ValidationError = require('../errors/ValidationError');
 
 module.exports = (app) => {
   const find = (filter = {}) => app.db('transfers')
@@ -6,6 +6,18 @@ module.exports = (app) => {
     .select();
 
   const save = async (transfer) => {
+    if (!transfer.description) throw new ValidationError('Descrição é um atributo obrigatório!');
+    if (!transfer.date) throw new ValidationError('Data é um atributo obrigatório!');
+    if (!transfer.ammount) throw new ValidationError('Valor é um atributo obrigatório!');
+    if (!transfer.acc_ori_id) throw new ValidationError('Conta de origem é um atributo obrigatório!');
+    if (!transfer.acc_dest_id) throw new ValidationError('Conta de destino é um atributo obrigatório!');
+    if (transfer.acc_ori_id === transfer.acc_dest_id) throw new ValidationError('Não é possível transferir para a mesma conta!');
+
+    const accounts = await app.db('accounts').whereIn('id', [transfer.acc_ori_id, transfer.acc_dest_id]);
+    accounts.forEach((acc) => {
+      if (acc.user_id !== parseInt(transfer.user_id, 10)) throw new ValidationError('Conta #10002 não pertence ao usuário!');
+    });
+
     const result = await app.db('transfers').insert(transfer, '*');
     const transferId = result[0].id;
 
