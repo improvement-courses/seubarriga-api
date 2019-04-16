bcb2933711dcdd2ce3710f74507fb41b6bcaf79d
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-04-16 15:23:10 -0300

    Deve listar apenas as transferências do usuário

--- test/routes/transfer.test.js
+++ test/routes/transfer.test.js
@@ -6,8 +6,8 @@ const MAIN_ROUTE = '/v1/transfers';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwMDAsIm5hbWUiOiJVc2VyICMxIiwiZW1haWwiOiJ1c2VyMUBnbWFpbC5jb20ifQ.4IxZIO6DC43OUiXDbq-UbBZkDPWTKwnqoPKqhn-sfoo';
 
 beforeAll(async () => {
-  await app.db.migrate.rollback();
-  await app.db.migrate.latest();
+  // await app.db.migrate.rollback();
+  // await app.db.migrate.latest();
   await app.db.seed.run();
 });
 
