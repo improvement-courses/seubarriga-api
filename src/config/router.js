2651a176b315fac8f1e49df40c7ddab60bc7c81b
Williams Gomes <williams.gomes@a.ficr.edu.br>
2019-04-25 15:27:51 -0300

    Final

--- src/config/router.js
+++ src/config/router.js
@@ -12,6 +12,6 @@ module.exports = (app) => {
 
   app.use('/v1', app.config.passport.authenticate(), protectedRouter);
 
-  app.get('/v2/users', (req, res) => res.status(200).send('V2 no ar'));
+  // app.get('/v2/users', (req, res) => res.status(200).send('V2 no ar'));
   app.use('/v2', protectedRouter);
 };
